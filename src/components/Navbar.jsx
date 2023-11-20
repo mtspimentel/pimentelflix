4d864bc1cfd37a9848c9dcdd2e8a6af7de57a806
Matheus <mtspimentel@icloud.com>
2023-11-20 20:45:06 -0300

    pimentel-flix

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {BiCameraMovie, BiSearchAlt2} from 'react-icons/bi';
+import './Navbar.css';
+
+const Navbar = () => {
+    const [search, setSearch] = useState ('')
+    
+    const navigate = useNavigate();
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        
+        if(!search) return
+        navigate(`/search?q=${search}`)
+        setSearch(''); 
+    };
+    return (
+        <nav id="navbar">
+            <h2>
+                <Link to='/'><BiCameraMovie /> PimentelFlix </Link>
+            </h2>
+            <form onSubmit={handleSubmit}>
+                <input type='text' 
+                placeholder="Busque um filme" 
+                onChange={(e) => setSearch(e.target.value)}
+                value={search}
+                />
+            <button type="submit"></button>
+                <BiSearchAlt2/>
+            </form>
+        </nav>
+    )
+}
+
+export default Navbar;
